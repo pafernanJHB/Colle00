72b472067cdd81681f8ea02274638856f9bab62d
Paulo Fernandes <pafernan@c4r23s5.wethinkcode.co.za>
2020-07-15 10:59:59 +0200

    Rush00

--- ex00/Rush00/rush00.c
+++ ex00/Rush00/rush00.c
@@ -6,40 +6,49 @@
 /*   By: pafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 10:10:49 by pafernan          #+#    #+#             */
-/*   Updated: 2020/07/15 10:28:52 by pafernan         ###   ########.fr       */
+/*   Updated: 2020/07/15 10:58:36 by pafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<unistd.h>
 #include	"ft_putchar.c"
 
-int		rush(int a)
+int		rush(int a, int b)
 {	
 	char c, d, e, f;
 	c = 'o';
 	d = '-';
 	e = '|';
 	f = ' ';
+	int t;
+	t = 1;
+	int t2;
+	t2 = 1;
 	
 	while (a > 0)
 	{
-		if (a == 1)
+		if (a == 1 || t > 0 )
 		{
 			ft_putchar(c);
+			a--;
+			t--;
 		}
-		else if (a > 1  && a < a - 1 )
+		else if (a > 1  || a < a - t2 )
 		{
 			ft_putchar(d);
+			a--;
+			t2--;
 		}
-		a--;
 	}
-	//while (b >= 0 )
-	//{
-	//	if (b > 1)
-	//	{
-	//		ft_putchar(c)
-	//	}
-	//}
+	while (b > 1 )
+	{
+		if (b == 1)
+		{
+			ft_putchar('\n');
+			ft_putchar(e);
+			b--;
+		}
+	}
 
 	return	(0);
 }
